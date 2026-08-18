5db27f811a5340a6998f5df41f11cde8d591061d
Ricardo Díaz <ricardotk002@gmail.com>
2026-08-18 13:03:43 -0400

    Implement traditional RoPE
    Damn this one was hard

--- src/tiny_llm/positional_encoding.py
+++ src/tiny_llm/positional_encoding.py
@@ -9,9 +9,41 @@ class RoPE:
         base: int = 10000,
         traditional: bool = False,
     ):
-        pass
+        self.dims = dims
+        self.seq_len = seq_len
+        self.base = base
+        self.traditional = traditional
+
+        self.theta = 1 / (base ** (mx.arange(0, self.dims, 2) / self.dims))
+
+        self.seq_idx = mx.arange(seq_len)
+
+        self.idx_theta = self.seq_idx[:, None] * self.theta[None, :] # einsum
+
+        self.cache = mx.stack([self.idx_theta.cos(), self.idx_theta.sin()], axis=-1)
 
     def __call__(
         self, x: mx.array, offset: list[slice] | slice | None = None
     ) -> mx.array:
-        pass
+
+        seq_len = x.shape[1]
+
+        if offset is not None:
+            if isinstance(offset, slice):
+                cache = self.cache[offset]
+            else:
+                cache = self.cache[:offset]
+        else:
+            cache = self.cache[:seq_len]
+
+        x_shaped = mx.reshape(x, (*x.shape[:-1], -1, 2))
+        cache_shaped = mx.reshape(cache, (-1, x_shaped.shape[1], 1, x_shaped.shape[3], 2))
+
+        x_out = mx.stack([
+            x_shaped[..., 0] * cache_shaped[..., 0]
+            - x_shaped[..., 1] * cache_shaped[..., 1],
+            x_shaped[..., 1] * cache_shaped[..., 0]
+            + x_shaped[..., 0] * cache_shaped[..., 1]
+        ], axis=-1)
+
+        return x_out.flatten(3)
