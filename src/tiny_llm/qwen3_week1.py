79fe13c4ce8a65da235bfd8c9e9cc0bd14279cfd
Ricardo Díaz <ricardotk002@gmail.com>
2026-08-19 15:37:11 -0400

    Implement Qwen3 Grouped Query Attention
    One issue I had: Qwen3MultiHeadAttention input tensors are in a different shape
    then what scaled_dot_product_attention_grouped accepts, so transposes are needed
    
    in a different shape th

--- src/tiny_llm/qwen3_week1.py
+++ src/tiny_llm/qwen3_week1.py
@@ -25,15 +25,52 @@ class Qwen3MultiHeadAttention:
         theta: int = 1000000,
         rms_norm_eps: float = 1e-5,
     ):
-        pass
+        self.hidden_size = hidden_size
+        self.num_heads = num_heads
+        self.num_kv_heads = num_kv_heads
+        self.head_dim = head_dim
+        self.wq = wq
+        self.wk = wk
+        self.wv = wv
+        self.wo = wo
+        self.q_norm = q_norm
+        self.k_norm = k_norm
+        self.max_seq_len = max_seq_len
+        self.theta = theta
+        self.rms_norm_eps = rms_norm_eps
 
     def __call__(
         self,
         x: mx.array,
         mask: mx.array | str | None = None,
     ) -> mx.array:
-        pass
+        L = x.shape[-2]
 
+        q = linear(x, self.wq)
+        k = linear(x, self.wk)
+        v = linear(x, self.wv)
+
+        q = mx.reshape(q, (*q.shape[:-1], self.num_heads, -1))
+        k = mx.reshape(k, (*k.shape[:-1], self.num_kv_heads, -1))
+        v = mx.reshape(v, (*v.shape[:-1], self.num_kv_heads, -1))
+
+        q = mx.fast.rms_norm(q, self.q_norm, eps=self.rms_norm_eps)
+        k = mx.fast.rms_norm(k, self.k_norm, eps=self.rms_norm_eps)
+
+        q = RoPE(self.head_dim, self.max_seq_len, self.theta)(q, slice(0, L))
+        k = RoPE(self.head_dim, self.max_seq_len, self.theta)(k, slice(0, L))
+
+        q = q.swapaxes(-3,-2)
+        k = k.swapaxes(-3,-2)
+        v = v.swapaxes(-3,-2)
+
+        x = scaled_dot_product_attention_grouped(q, k, v, None, mask)
+
+        x = x.swapaxes(-3,-2)
+        x = mx.reshape(x, (*x.shape[:-2], -1))
+
+
+        return linear(x, self.wo)
 
 class Qwen3MLP:
     def __init__(
