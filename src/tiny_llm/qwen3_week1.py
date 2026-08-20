08c7a0bcffbcdce8037189a76fe8082a2a32b3da
Ricardo Díaz <ricardotk002@gmail.com>
2026-08-19 23:03:48 -0400

    Implement Qwen3MLP
    I still don't know what to do with dim/hidden_dim parameters?

--- src/tiny_llm/qwen3_week1.py
+++ src/tiny_llm/qwen3_week1.py
@@ -81,10 +81,16 @@ class Qwen3MLP:
         w_up: mx.array,
         w_down: mx.array,
     ):
-        pass
+        self.dim = dim
+        self.hidden_dim = hidden_dim
+        self.w_gate = w_gate
+        self.w_up = w_up
+        self.w_down = w_down
 
     def __call__(self, x: mx.array) -> mx.array:
-        pass
+        gate = silu(linear(x, self.w_gate))
+        up = linear(x, self.w_up)
+        return linear(gate * up, self.w_down)
 
 
 class Qwen3TransformerBlock:
