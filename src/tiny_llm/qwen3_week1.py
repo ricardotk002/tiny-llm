66ad8c0eae2c41a1f68ee539a9db02a3a99dd31a
Ricardo Díaz <ricardotk002@gmail.com>
2026-08-20 23:56:14 -0400

    Implement Qwen3 base model
    A few things that were confusing:
    
    - embeddings and projections were quantized (this is mentioned in the book,
    but I overlooked it)
    - model.parameters() gives you a JSON representation. in order to access the
    tensors you have to do it through mlx_model.model.<layer>

--- src/tiny_llm/qwen3_week1.py
+++ src/tiny_llm/qwen3_week1.py
@@ -173,10 +173,98 @@ class Qwen3TransformerBlock:
 
 class Qwen3ModelWeek1:
     def __init__(self, mlx_model: Any):
-        pass
+        self.mlx_model = mlx_model
+        self.vocab_size = mlx_model.args.vocab_size
+        self.hidden_size = mlx_model.args.hidden_size
+        self.num_attention_heads = mlx_model.args.num_attention_heads
+        self.num_kv_heads = mlx_model.args.num_key_value_heads
+        self.head_dim = mlx_model.args.head_dim
+        self.intermediate_size = mlx_model.args.intermediate_size
+        self.rms_norm_eps = mlx_model.args.rms_norm_eps
+        self.theta = mlx_model.args.rope_theta
 
     def __call__(
         self,
         inputs: mx.array,
     ) -> mx.array:
-        pass
+        # Args:
+        #
+        # ModelArgs(
+        #   model_type='qwen3',
+        #   hidden_size=2560,
+        #   num_hidden_layers=36,
+        #   intermediate_size=9728,
+        #   num_attention_heads=32,
+        #   rms_norm_eps=1e-06,
+        #   vocab_size=151936,
+        #   num_key_value_heads=8,
+        #   max_position_embeddings=65536,
+        #   rope_theta=1000000,
+        #   head_dim=128,
+        #   tie_word_embeddings=True,
+        #   rope_scaling=None
+        #  )
+        #
+        # Structure:
+        #
+        # model
+        #   embed_tokens
+        #       weight
+        #       scales
+        #       biases
+        #   layers x 28
+        #       self_attn
+        #         q_proj
+        #         k_proj
+        #         v_proj
+        #         o_proj
+        #         q_norm
+        #         k_norm
+        #         rope (empty)
+        #       mlp
+        #         gate_proj
+        #         down_proj
+        #         up_proj
+        #       input_layernorm
+        #       post_attention_layernorm
+        #   norm
+
+        x = Embedding(
+            vocab_size=self.vocab_size,
+            embedding_dim=self.hidden_size,
+            weight=dequantize_linear(self.mlx_model.model.embed_tokens)
+        )(inputs)
+
+
+        for layer in self.mlx_model.layers:
+            x = Qwen3TransformerBlock(
+                num_attention_heads=self.num_attention_heads,
+                num_kv_heads=self.num_kv_heads,
+                hidden_size=self.hidden_size,
+                head_dim=self.head_dim,
+                intermediate_size=self.intermediate_size,
+                rms_norm_eps=self.rms_norm_eps,
+                wq=dequantize_linear(layer.self_attn.q_proj),
+                wk=dequantize_linear(layer.self_attn.k_proj),
+                wv=dequantize_linear(layer.self_attn.v_proj),
+                wo=dequantize_linear(layer.self_attn.o_proj),
+                q_norm=layer.self_attn.q_norm.weight,
+                k_norm=layer.self_attn.k_norm.weight,
+                w_gate=dequantize_linear(layer.mlp.gate_proj),
+                w_up=dequantize_linear(layer.mlp.up_proj),
+                w_down=dequantize_linear(layer.mlp.down_proj),
+                w_input_layernorm=layer.input_layernorm.weight,
+                w_post_attention_layernorm=layer.post_attention_layernorm.weight,
+                theta=self.theta
+            )(x, mask="causal")
+
+        x = RMSNorm(self.head_dim, self.mlx_model.model.norm.weight, eps=self.rms_norm_eps)(x)
+
+        x = Embedding(
+            vocab_size=self.vocab_size,
+            embedding_dim=self.hidden_size,
+            weight=dequantize_linear(self.mlx_model.model.embed_tokens)
+        ).as_linear(x)
+
+        return x
+
