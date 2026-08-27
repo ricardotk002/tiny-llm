2b0d24d734a7a4bacdd173151edb9a19e0ed2729
Ricardo Díaz <ricardotk002@gmail.com>
2026-08-27 15:24:31 -0400

    Implement top_k sampling

--- src/tiny_llm/sampler.py
+++ src/tiny_llm/sampler.py
@@ -6,6 +6,15 @@ def make_sampler(temp: float, top_p: float | None, top_k: int | None):
     def sample(logprobs: mx.array):
         if temp == 0:
             return mx.argmax(logprobs, axis=-1)
-        pass
+
+        if temp > 0:
+            if top_k is not None:
+                top = mx.argpartition(logprobs, -top_k, axis=-1)[:,-top_k:]
+                masked_logits = mx.full_like(logprobs, -mx.inf)
+                masked_logits[:,top] = logprobs[:,top]
+
+                return mx.random.categorical(masked_logits / temp)
+
+            return mx.random.categorical(logprobs / temp)
 
     return sample
